eca7cfc0dbdd811fe79db08a1a2af4b42bb507e5
Jigchu <111167590+Jigchu@users.noreply.github.com>
2025-05-07 23:05:47 +0800

    Separated files and changed the commds to be stored in a linked list

---
+++ Calculator.py
@@ -0,0 +1,133 @@
+from __future__ import annotations
+
+class Calculator:
+    def __init__(self, command: str):
+        self.commands: Token = self.parse(command)
+
+    # TODO: Add functions and inlines and stuff
+    def parse(self, command: str):
+        commands: Token = Token()
+        
+        # Parse additions first
+        current = commands
+        for segment in command.split(sep="+"):
+            current.next = Token(segment)
+            current.next.next = Token("+")
+            current = current.next.next
+
+        _ = commands.pop()
+
+        # Parse multiplication
+        current = commands
+        prev = None
+        while current is not None:
+            if "*" not in current.value:
+                prev = current
+                current = current.next
+                continue
+            negative_count = current.value.count("-")
+            current.value = current.value.replace("-", "")
+            if negative_count % 2 == 0:
+                prev = current
+                current = current.next
+                continue
+            if prev is None:
+                prev = current
+                current = current.next
+                continue
+            new_token = Token("-", current)
+            prev.next = new_token
+            prev = current
+            current = current.next
+
+        # Parse negatives
+        current = commands
+        prev = None
+        while current is not None:
+            if "-" not in current.value:
+                prev = current
+                current = current.next
+                continue
+            if len(current.value) == 1:
+                prev = current
+                current = current.next
+                continue
+
+            token = f"\n{current.value}\n"
+            token = token.split("-")
+            for i in range(len(token)-1, 0, -1):
+                token.insert(i, "-")
+            token = [term for term in token if term != "\n"]
+            token = list(map(lambda s: s.strip(), token))
+
+            buffer = Token(token[0])
+            tmp = buffer
+            for term in token[1:]:
+                new_token = Token(term)
+                tmp.next = new_token
+                tmp= new_token
+
+            if prev is None:
+                break
+
+            prev.next = buffer
+            tmp.next = current.next
+
+            prev = tmp
+            current = current.next
+
+        return commands
+    
+    def run(self, starting_value: int = 0):
+        curr_number = starting_value
+        direction: int = 1
+
+        current = self.commands.next
+        if current is None:
+            return curr_number
+
+        while current is not None:
+            command = current.value
+            match command:
+                case "+":
+                    pass
+                case "-":
+                    direction *= -1
+                case command if any([c for c in command if c == "*"]):
+                    command = command.split("*")
+                    curr_number += direction * int(command[0]) * int(command[1])
+                case _:
+                    curr_number += direction * int(command)
+            current = current.next
+        
+        return curr_number
+
+
+# TODO: Make a very fancy iterator for my very fancy linked list
+
+# Fancy class that is just a linked list
+class Token:
+    def __init__(self, value: str = "", next: Token | None = None):
+        self.value: str = value
+        self.next: Token | None = next
+
+    # Removes the last element of the linked list and returns its value or None
+    def pop(self):
+        if self.next is None:
+            return None
+        
+        current = self
+        while current.next is not None:
+            if current.next.next is None:
+                break
+            current = current.next
+
+
+        last = current.next
+        if last is None:
+            return None
+        
+        current.next = None
+
+        return last.value
+
--- calc.py
+++ calc.py
@@ -1,97 +1,20 @@
 import sys
 
+from Calculator import Calculator
 
+# TODO: Create syntax checking and flags and others
 def main():
+    starting_value = 0
+
     while True:
-        diagnostic = calculator()
+        command = input("> ").strip().replace(" ", "")
+        if command == "quit":
+            break
 
-        match diagnostic:
-            case 0:
-                pass
-            case 1:
-                break
-            case _:
-                pass
+        calculator = Calculator(command)
+        print(calculator.run(starting_value))
 
     return 0
-
-
-def calculator(**flags):
-    command = input("> ").strip().replace(" ", "")
-    if command == "quit":
-        return 1
-
-    command = f"0+{command}"
-    command_segments = parse(command)
-    print(run(command_segments))
-
-    return 0
-
-
-def parse(command: str):
-    command_segments: list[str] = []
-
-    # Parse additions first
-    for segment in command.split(sep="+"):
-        command_segments.append(segment)
-        command_segments.append("+")
-
-    _ = command_segments.pop()
-
-    # Parse multiplication
-    inserted: bool = False
-    for index, segment in enumerate(command_segments):
-        if inserted:
-            inserted = False
-            continue
-        if "*" not in segment:
-            continue
-        negative_count = segment.count("-")
-        segment = segment.replace("-", "")
-        if negative_count % 2 == 1:
-            command_segments.insert(index, "-")
-            inserted = True
-            index += 1
-        command_segments[index] = segment
-
-    # Parse negatives
-    for index, segment in enumerate(command_segments):
-        if "-" not in segment:
-            continue
-        if len(segment) == 1:
-            continue
-        segment = f"\n{segment}\n"
-        segment = segment.split(sep="-")
-        for i in range(len(segment)-1, 0, -1):
-            segment.insert(i, "-")
-        segment = [term for term in segment if term != "\n"]
-        segment = list(map(lambda x: x.strip(), segment))
-        buffer = command_segments[index+1:]
-        command_segments = command_segments[:index]
-        command_segments.extend(segment)
-        command_segments.extend(buffer)
-
-    return command_segments
-
-
-def run(command_segments: list[str]):
-    curr_number = int(command_segments.pop(0))
-    direction: int = 1
-
-    for command in command_segments:
-        match command:
-            case "+":
-                pass
-            case "-":
-                direction *= -1
-            case command if any([c for c in command if c == "*"]):
-                command = command.split("*")
-                curr_number += direction * int(command[0]) * int(command[1])
-            case _:
-                curr_number += direction * int(command)
-
-    return curr_number
-
 
 if __name__ == "__main__":
     try:
